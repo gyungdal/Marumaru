83fcaed7a8676f6c540cab79b8d65bd34bbaf37e
Kim GyungSik <Kim GyungSik>
2016-05-24 17:39:08 +0900

    class process...

--- app/src/main/java/com/gyungdal/marumaru/MainActivity.java
+++ app/src/main/java/com/gyungdal/marumaru/MainActivity.java
@@ -1,23 +1,60 @@
 package com.gyungdal.marumaru;
 
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.gyungdal.marumaru.image.ImageDownload;
+import com.gyungdal.marumaru.page.PageTitle;
 
 public class MainActivity extends AppCompatActivity {
     public static String comicTitle;
+    private ClipboardManager clipboardManager;
+    private PageTitle title;
+    private ImageDownload download;
+    private EditText eurl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
+    private void init(){
+        title = new PageTitle();
+        download = new ImageDownload(getApplicationContext());
+        eurl = (EditText)findViewById(R.id.url);
+        clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+    }
     private boolean isOnline() {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    public void Click(View v){
+        switch(v.getId()){
+            case R.id.fab :
+                if(isOnline()) {
+                    if(eurl.getText().toString().contains("marumaru.in/b/manga")){
+
+                    }
+                }else{
+                    Snackbar.make(getCurrentFocus(), "온라인 상태가 아닙니다.",
+                            Snackbar.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.copy :
+                eurl.setText(clipboardManager.getText().toString());
+                break;
+            default : break;
+        }
     }
 }
---
+++ app/src/main/java/com/gyungdal/marumaru/page/PageTitle.java
@@ -0,0 +1,37 @@
+package com.gyungdal.marumaru.page;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.Source;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by GyungDal on 2016-05-24.
+ */
+public class PageTitle extends AsyncTask<String, Void, String>{
+    @Override
+    protected String doInBackground(String... params) {
+        String result = "";
+        try {
+            Source source = new Source(new URL(params[0]));
+            source.fullSequentialParse();
+            List<Element> list = source.getAllElements(HTMLElementName.H1);
+            for(Element element : list){
+                String temp = element.getTextExtractor().toString();
+                result = temp.substring(0, temp.lastIndexOf(" "));
+            }
+        }catch (MalformedURLException eurl){
+            Log.e("Get page url", eurl.getMessage());
+        }catch (IOException eio){
+            Log.e("Get page url", eio.getMessage());
+        }
+        return result;
+    }
+}
